f2ba397d9b85cc16f3eaaa3e5a5cbd9b987134a0
massu-159 <magjtwjp@gmail.com>
2023-04-14 23:59:59 +0900

    refactor ts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React from 'react'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
+      <Head children={undefined}>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
